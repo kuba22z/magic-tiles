cec87e8d1bee3cb6318cc8d126ff2c28626bb19e
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-04-17 01:03:37 +0200

    style-guide.md index.vue creates basic layout for landing page. moves test pages to separate folder. creates first version of style guide

--- magic-tiles/pages/index.vue
+++ magic-tiles/pages/index.vue
@@ -1,25 +1,37 @@
 <template>
-    <div class="container">
-        <div>
-            <Logo />
-            <h1 class="title">magic-tiles</h1>
-            <div class="links">
-                <a
-                    href="https://nuxtjs.org/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="button--green"
+    <div>
+        <div
+            class="flex flex-col justify-center items-center border border-red-300"
+        >
+            <div
+                class="explanation-top border flex justify-center items-center border-red-300 my-10 w-full text-center"
+            >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate, inventore eligendi. Saepe odit consequuntur
+                consectetur iste? Doloremque soluta eius nobis, distinctio,
+            </div>
+            <div
+                class="flex flex-col justify-center items-center logo-and-hint mt-1 w-full border border-red-300"
+            >
+                <div class="image h-20 w-20 bg-green-300"></div>
+                <div class="hint text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Illum totam autem reprehender
+                </div>
+            </div>
+            <div
+                class="bottom-navigation-buttons flex flex-col justify-center mt-10 items-center border border-red-300 w-full"
+            >
+                <div
+                    class="next-button border border-red-300 h-12 w-24 flex justify-center items-center text-center"
                 >
-                    Documentation
-                </a>
-                <a
-                    href="https://github.com/nuxt/nuxt.js"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="button--grey"
+                    weiter
+                </div>
+                <div
+                    class="back-button mt-6 border border-red-300 h-12 w-24 flex justify-center items-center text-center"
                 >
-                    GitHub
-                </a>
+                    back to street!
+                </div>
             </div>
         </div>
     </div>
@@ -31,41 +43,4 @@ import Vue from "vue";
 export default Vue.extend({});
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-}
-
-.title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system,
-        BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
-        sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-}
-
-.subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-}
-
-.links {
-    padding-top: 15px;
-}
-</style>
+<style scoped></style>
