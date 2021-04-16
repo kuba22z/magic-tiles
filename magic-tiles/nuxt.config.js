8ea346b3134dcfdba49b3324f979644a88d71171
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-04-16 19:32:25 +0200

    removes .prettierrc use prettier.config.js instead. sets tabwidth to 4. trailingcomme to es5 and semicolon to true. reformats files with prettier. adds test.vue page for testing

--- magic-tiles/nuxt.config.js
+++ magic-tiles/nuxt.config.js
@@ -1,44 +1,52 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'magic-tiles',
-    htmlAttrs: {
-      lang: 'en',
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "magic-tiles",
+        htmlAttrs: {
+            lang: "en",
+        },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            { hid: "description", name: "description", content: "" },
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        "@nuxt/typescript-build",
+        // https://go.nuxtjs.dev/tailwindcss
+        "@nuxtjs/tailwindcss",
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        "@nuxtjs/axios",
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+    // https://tailwindcss.nuxtjs.org/releases/#v4.0.0
+    tailwindcss: {
+        jit: true,
+    },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-}
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+};
---
+++ magic-tiles/pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="wrap flex items-center justify-center">
+            <h1>header one</h1>
+            <div class="text">hello world</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
