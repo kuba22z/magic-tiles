162a4a6df2a0b17b4dbd0aed4e7db61392b96f0c
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-04-16 20:19:11 +0200

    adds vue-class-components and vue-property-decorator to use class-based components. creates default tailwindcss config file.

---
+++ magic-tiles/tailwind.config.js
@@ -0,0 +1,12 @@
+// default tailwindcss configuration file. created with "npx tailwindcss init"
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
