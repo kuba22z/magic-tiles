c6d557a03040ec8c9b2d8515e492a449e3e04384
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-04-16 21:06:42 +0200

    implements small example component using props

--- magic-tiles/pages/classTestGithub.vue
+++ magic-tiles/pages/classTestGithub.vue
@@ -1,17 +1,22 @@
 <template>
-    <div
-        class="screen-counter flex flex-col w-full items-center justify-center"
-    >
-        <h1 class="text-lg font-semibold">Hello counter world</h1>
-        <div class="mt-10 flex flex-col justify-center items-center">
-            <div class="counter">Counter : {{ counter }}</div>
-            <div
-                class="counter-buttons flex w-full space-x-10 justify-center items-center"
-            >
-                <button @click="increment">Increment</button>
-                <button @click="decrement">Decrement</button>
+    <div>
+        <!-- counter -->
+        <div
+            class="screen-counter flex flex-col w-full items-center justify-center"
+        >
+            <h1 class="text-lg font-semibold">Hello counter world</h1>
+            <div class="mt-10 flex flex-col justify-center items-center">
+                <div class="counter">Counter : {{ counter }}</div>
+                <div
+                    class="counter-buttons flex w-full space-x-10 justify-center items-center"
+                >
+                    <button @click="increment">Increment</button>
+                    <button @click="decrement">Decrement</button>
+                </div>
             </div>
         </div>
+        <!-- random button -->
+        <magicTilesButton :button-caption="`click me!`" />
     </div>
 </template>
 
---
+++ magic-tiles/types/README.md
@@ -0,0 +1,3 @@
+# This directory will be used for our typescript interfaces.
+
+- Whenever we define a new type/interface, create a separate file here.
