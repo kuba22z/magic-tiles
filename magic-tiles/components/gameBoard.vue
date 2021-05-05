8476dcfd5808359fdc7e06ecf099c405746c94c9
kuba22z <your@email.com>
2021-05-05 17:47:46 +0200

    gameRectangle.vue: creates component with an image

--- magic-tiles/components/gameBoard.vue
+++ magic-tiles/components/gameBoard.vue
@@ -1,21 +1,15 @@
 <template>
     <div>
         <svg height="428" width="282" class="border border-blue-600">
-            <rect height="25%" width="25%" y="0"></rect>
             <rect height="25%" width="25%" y="25%"></rect>
             <rect height="25%" width="25%" y="50%"></rect>
             <rect height="25%" width="25%" y="75%" fill="red"></rect>
 
             <rect height="25%" width="25%" x="50%"></rect>
             <rect height="25%" width="25%" x="75%"></rect>
-            <rect
-                height="25%"
-                width="25%"
-                :y="rect.y + '%'"
-                :x="rect.x + '%'"
-                fill="red"
-            />
+            <GameRectangle :rect="rect" />
         </svg>
+
         <button @click="startGame" class="test-btn border border-black w-40">
             Test
         </button>
@@ -26,11 +20,14 @@
 import { Component, Prop, Watch, Vue } from "vue-property-decorator";
 import { namespace } from "vuex-class";
 import { Rect } from "types/gaming-screen";
+import GameRectangle from "~/components/gameRectangle.vue";
+import Test from "~/pages/testing/test.vue";
 
 const board = namespace("game-board");
-@Component
+@Component({
+    components: { Test, GameRectangle },
+})
 export default class gamingBoard extends Vue {
-    public localData: object = {};
     @board.State
     public rect!: Rect;
 
---
+++ magic-tiles/components/gameRectangle.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg :y="rect.y + '%'" :x="rect.x + '%'">
+        <rect fill="red" height="25%" width="25%" />
+        <image
+            href="https://mdn.mozillademos.org/files/6457/mdn_logo_only_color.png"
+            height="25%"
+            width="25%"
+        />
+    </svg>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+import { Rect } from "types/gaming-screen";
+
+@Component
+export default class gamingRectangle extends Vue {
+    @Prop() public rect!: Rect;
+}
+</script>
+
+<style scoped></style>
