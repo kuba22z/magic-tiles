d3e62c882a5f4e8c539f0ce7529d400197a964f2
kuba22z <your@email.com>
2021-05-05 23:00:55 +0200

    gameRow.vue: creates component, gaming-board.ts: initialize rectRow with values, creates new type RectRow

---
+++ magic-tiles/components/gameRow.vue
@@ -0,0 +1,23 @@
+<template>
+    <g>
+        <GameRectangle
+            v-for="rect in rectRow.row"
+            :rect="rect"
+            v-bind:key="rect.x"
+        />
+    </g>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { RectRow } from "types/gaming-screen";
+import GameRectangle from "~/components/gameRectangle.vue";
+@Component({
+    components: { GameRectangle },
+})
+export default class gamingRectangle extends Vue {
+    @Prop() public rectRow!: RectRow;
+}
+</script>
+
+<style scoped></style>
