c6311d6f3bd668dc5263ee790df07383aa10e6da
kuba22z <your@email.com>
2021-05-07 11:55:20 +0200

    gameBoard.vue: use requestAnimationFrame()+setTimeout() as gameloop, create function stopGame()

--- nuxt-client/components/gameBoard.vue
+++ nuxt-client/components/gameBoard.vue
@@ -4,8 +4,11 @@
             <GameRow v-for="rectRow in rectBoard.array" :rect-row="rectRow" />
         </svg>
 
-        <button class="test-btn border border-black w-40" @click="initStart(30)">
+        <button class="test-btn border border-black w-40" @click="startGame">
             Test
+        </button>
+        <button class="test-btn border border-black w-20" @click="stopGame">
+            Stop
         </button>
     </div>
 </template>
@@ -17,10 +20,11 @@ import { Rect, RectArray } from "~/types/interfaces/gaming-screen";
 import GameRectangle from "~/components/gameRectangle.vue";
 import Test from "~/pages/testing/test.vue";
 import GameRow from "~/components/gameRow.vue";
-var i=0;
-var stepSize =2.5,bigStep=25/stepSize ;
-var fpsInterval :number ,startTime :number, now :number, then :number, elapsed :number;
-
+let i = 0;
+let stepSize = 2.5,
+    bigStep = 25 / stepSize;
+let start_ref: number;
+let timer: any;
 
 const board = namespace("game-board");
 @Component({
@@ -36,61 +40,28 @@ export default class gamingBoard extends Vue {
     @board.Mutation
     public pushFrontAndPop!: () => void;
 
-/*
-  public useInterval(callback, delay) {
-    const savedCallback = useRef();
-
-    // Remember the latest function.
-    useEffect(() => {
-      savedCallback.current = callback;
-    }, [callback]);
-
-    // Set up the interval.
-    useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    }, [delay]);
-  }
-*/
     /**
      *@describe Endless loop which move all RectRow to the bottom
      * and creates new rectRows at the top
      */
     public startGame() {
-      // request another frame
-      //requsetAnimationFrame is a better endless loop for animations than setIntervall
-      requestAnimationFrame(this.startGame);
-
-      // calc elapsed time since last loop
-      now = Date.now();
-      elapsed = now - then;
-      // if enough time has elapsed, draw the next frame
-
-      if (elapsed > fpsInterval) {
-        // Get ready for next frame by setting then=now, but also adjust for your
-        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
-        then = now - (elapsed % fpsInterval);
-
+        timer = setTimeout(() => {
+            start_ref = requestAnimationFrame(this.startGame);
+        }, 25);
         this.moveRectRowDown(stepSize);
         i++;
         if (i === bigStep) {
-          this.pushFrontAndPop();
-          i = 0;
+            this.pushFrontAndPop();
+            i = 0;
         }
-      }
     }
-  public initStart(fps :number) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    this.startGame();
-  }
 
+    /**
+     * @describe stop the game
+     */
+    public stopGame() {
+        cancelAnimationFrame(start_ref);
+        clearTimeout(timer);
+    }
 }
 </script>
