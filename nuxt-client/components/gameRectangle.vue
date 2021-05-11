380be170e3edc837fff4132842f0ab65be50123c
kuba22z <your@email.com>
2021-05-11 20:16:10 +0200

    gameRectangle.vue: tries to fix bug with :href=rect.image

--- nuxt-client/components/gameRectangle.vue
+++ nuxt-client/components/gameRectangle.vue
@@ -1,19 +1,8 @@
 <template>
     <svg :x="rect.x + '%'" :y="rect.y + '%'">
         <rect fill="black" height="25%" width="25%" />
-        <!--    TODO(jakub) BUG: :href="rect.image" doesnt work properly-->
-        <image
-            v-if="rect.image === 'book.svg'"
-            href="~/assets/book.svg"
-            height="25%"
-            width="25%"
-        />
-        <image
-            v-if="rect.image === 'cook-book.svg'"
-            href="~/assets/cook-book.svg"
-            height="25%"
-            width="25%"
-        />
+        <!--    TODO(jakub) BUG: on firefox some images flicker between cook-book.svg and book.svg -->
+        <image :href="'_nuxt/assets/' + rect.image" height="25%" width="25%" />
     </svg>
 </template>
 
