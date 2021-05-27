5fbdb1d496fbdf9893150bf993b78cd346e5eae6
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-05-27 16:32:20 +0200

    try using default cypress cache path instead.

--- .pipeline-scripts/test-e2e.sh
+++ .pipeline-scripts/test-e2e.sh
@@ -6,14 +6,7 @@
 # Description: Runs the end to end tests.
 
 
-# makes sure the cypress cache will be stored inside /nuxt-client.
-# This cache will be used in our gitlab ci.
-CYPRESS_CACHE_FOLDER="./cache/Cypress"
-
 cd /nuxt-client || (echo "/nuxt-client not found." && exit 1)
-
-echo "cypress cache folder: ..."
-npx run cypress cache path
 
 # makes sure the production server will be reachable under localhost
 echo 'HOST_ADDR="localhost"' > .env
