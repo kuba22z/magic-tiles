511886baf0fee119ce15c4cd4f88a1a02bcf9a55
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-05-27 17:39:26 +0200

    removes debugging output that was used to get cypress cache working.

--- .pipeline-scripts/build.sh
+++ .pipeline-scripts/build.sh
@@ -3,21 +3,20 @@
 # Author: Pierre Dahmani
 # Created: 29.04.2021
 #
-# Description: builds the nuxt client.
+# Description: copies cypress binary into build path and builds the nuxt client
 
 
 cd /nuxt-client || (echo "/nuxt-client not found." && exit 1)
 # installs all dependencies
 echo "installing dependencies..."
 npm ci
-# copies the cypress binary to build path because we instead got error
-# messages from gitlab ci
+
+# copies the cypress binary which will be cached to build path.
+# reason: 'Both artifacts and caches define their paths relative to the
+# project directory, and can’t link to files outside it.'
 echo "copying cypress binary to ./nuxt-client/.cache/Cypress"
 mkdir -p .cache
 cp -R ~/.cache/Cypress .cache/
-echo "resulting files in .cache:"
-ls -lah .cache
+
 echo "building nuxt client..."
 npm run build
-echo "try running cypress test here:"
-CYPRESS_CACHE_FOLDER=./.cache/Cypress npm run test:e2e
