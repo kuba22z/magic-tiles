e0d32f30550f83acc8c17557ca1be9c315c1e578
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-05-27 15:42:19 +0200

    use the build cache from the build job for our unit and end to end tests. creates separate e2e test and unit test script.

--- .pipeline-scripts/test-e2e.sh
+++ .pipeline-scripts/test-e2e.sh
@@ -3,7 +3,7 @@
 # Author: Pierre Dahmani
 # Created: 30.04.2021
 #
-# Description: stub. will be replaced by out unit and e2e tests.
+# Description: Runs the end to end tests.
 
 
 cd /nuxt-client || (echo "/nuxt-client not found." && exit 1)
@@ -11,10 +11,6 @@ cd /nuxt-client || (echo "/nuxt-client not found." && exit 1)
 # makes sure the production server will be reachable under localhost
 echo 'HOST_ADDR="localhost"' > .env
 
-# installing all packages and build server
-# NOTE(pierre): (somehow did not work with cache in gitlab ci)
-npm ci
-npm run build
-
 echo "Running e2e tests ..."
 npm run test:e2e
+echo "E2e tests finished."
